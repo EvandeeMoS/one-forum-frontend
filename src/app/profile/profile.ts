33ea801d07aa94ccf0eab6e60fe8186d4bf6625a
Kaue-C-Matos <kauecostamatos31@gmail.com>
2025-11-20 16:18:46 -0300

    Finalizada estrutura da tela de perfil

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-profile',
-  imports: [MatCardModule, MatIconModule, MatButtonModule, MatInputModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule, MatInputModule, MatSlideToggleModule, MatDivider, MatSelectModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
