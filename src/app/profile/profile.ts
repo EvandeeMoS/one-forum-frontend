2de5b1d2e8e8b5d9014d8f4710a6a7f8d8fa8acb
Kaue-C-Matos <kauecostamatos31@gmail.com>
2025-11-24 09:52:22 -0300

    adicionado modal para edição de perfil

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule, MatAnchor } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -14,5 +15,18 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
   styleUrl: './profile.css',
 })
 export class Profile {
+  readonly dialog = inject(MatDialog)
+
+  openDialog(){
+    this.dialog.open(EditDialog)
+  }
+}
+
+@Component({
+  selector: 'edit-dialog',
+  templateUrl: './edit-dialog.html',
+  imports: [MatDialogModule, MatInputModule, MatAnchor, MatButtonModule]
+})
+export class EditDialog{
 
 }
